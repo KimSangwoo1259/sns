cb0c455972574b19d99bb7e443e59dfa9a84059e
KimSangwoo12 <rlatkddn2902@gmail.com>
2023-10-06 15:14:27 +0900

    init : initiallize project
    first commit

---
+++ src/main/java/com/fastcampus/sns/SnsApplication.java
@@ -0,0 +1,14 @@
+package com.fastcampus.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) // db기본 설정 안하고 처음에 스프링을 띄우기 위해
+public class SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApplication.class, args);
+	}
+
+}
